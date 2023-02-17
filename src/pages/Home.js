fc2748e4d9d283e380eff0a00dae3cf8ad4fe2cb
unknown <acsgyorgy.mail@gmail.com>
2023-02-17 01:38:31 +0100

    picture added from backend server and set to responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,29 @@
 import '../css/Home.css';
 import NavigationBar from '../components/NavigationBar.js';
+//import { useState, useEffect } from "react";
 
 const Home = () => {
+
+
     return (
         <div className="home-page">
             <div>
                 <NavigationBar/>
             </div>
-            <div>
-                home
+            <div className="main">
+                <div className="coupon-code-container">
+                    <div>
+                        <img src={"http://localhost:4000/img/bookread.jpg"} className="coupon-code-picture" alt="book"/>
+                    </div>
+                    <div className="coupon-code-text-container">
+                        <div>
+                        "50% coupon code can be redeemed"
+                        </div>
+                    </div>
+                </div>
+                <div>                          
+                4 different newness
+                </div>
             </div>
         </div>
     );
