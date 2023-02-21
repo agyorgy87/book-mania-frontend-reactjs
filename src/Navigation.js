df9201a6a7a8154bee7e8709267bccb5bf9c655a
unknown <acsgyorgy.mail@gmail.com>
2023-02-21 14:17:47 +0100

    create provider and states for context

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,19 +1,26 @@
 import {Routes, Route, BrowserRouter } from "react-router-dom";
+import React, { useState } from "react";
 import Home from "./pages/Home.js";
 import Books from "./pages/Books.js";
 import Contact from "./pages/Contact.js";
-
+import { BookContext } from "./context/BookContext.js";
 
 const Navigation = () => {
 
+    const [bookData, setBookData] = useState([])
+
     return(
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/books" element={<Books/>} />
-                <Route path="/contact" element={<Contact/>} />
-            </Routes>
-        </BrowserRouter>
+        <div>
+            <BookContext.Provider value={{value:bookData, setValue:setBookData}}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Home/>} />
+                        <Route path="/books" element={<Books/>} />
+                        <Route path="/contact" element={<Contact/>} />
+                    </Routes>
+                </BrowserRouter>
+            </BookContext.Provider>
+        </div>
     )
 }
 
