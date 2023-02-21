6bda0927e5e4e6cb341a9aadfcbe8c39652da46f
unknown <acsgyorgy.mail@gmail.com>
2023-02-21 14:19:58 +0100

    context added to the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,24 @@
 import '../css/Home.css';
 import NavigationBar from '../components/NavigationBar.js';
-//import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import { useContext } from 'react';
+import { BookContext } from "../context/BookContext.js";
 
 const Home = () => {
 
+    const bookDetails = useContext(BookContext);
+
+    useEffect(() => {  
+        fetch("http:/localhost:4000/get-all-newness")
+            .then(data => data.json())
+            .then(parsedData => {
+                bookDetails.setValue(parsedData)
+        })
+    }, [])
+
+    console.log(bookDetails);
+
+    //axios
 
     return (
         <div className="home-page">
@@ -17,12 +32,24 @@ const Home = () => {
                     </div>
                     <div className="coupon-code-text-container">
                         <div>
-                        "50% coupon code can be redeemed"
+                            "50% coupon code can be redeemed"
                         </div>
                     </div>
                 </div>
-                <div>                          
+                <div>  
                 4 different newness
+                {
+                                    bookDetails.value.map((book, index) => (
+                                        <div>  
+                                            {book.title}
+                                            {book.genre}
+                                            {book.author}
+                                        </div>
+                                    ))
+                                }
+                </div>
+                <div>
+                books of more than 500 pages
                 </div>
             </div>
         </div>
