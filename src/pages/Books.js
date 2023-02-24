debbdae6feb65f848eb36806ecd0ca8cce837050
unknown <acsgyorgy.mail@gmail.com>
2023-02-24 12:16:29 +0100

    books genre selected done

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,22 +1,74 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import NavigationBar from '../components/NavigationBar.js';
 
 const Books = () => {
+
+    const allBooks = useRef([]);
+    const [visibleBooks, setVisibleBooks] = useState([]);
+    const [selectedBooksGenre, setSelectedBooksGenre] = useState([]);
+
+    useEffect(() => {  
+
+        fetch("http://localhost:4000/get-all-books")
+            .then(data => data.json())
+            .then(parsedData => {
+                allBooks.current = parsedData;
+                setVisibleBooks(parsedData);
+            })
+
+    }, [])
+
+
+    useEffect(() => { 
+
+        bookGenreSelection();
+
+    },[selectedBooksGenre])
+      
+    
+    const bookGenreSelection = () => {
+
+        let filterBooksGenre = allBooks.current
+        console.log(filterBooksGenre);
+        if(selectedBooksGenre !== "none"){   
+            filterBooksGenre = filterBooksGenre.filter(book => book.genre === selectedBooksGenre);
+            setVisibleBooks(filterBooksGenre); 
+        }
+        setVisibleBooks(filterBooksGenre);    
+    }
+
     return(
         <div>
             <div>
                 <NavigationBar/>
             </div>
-            <div>
-                <select>
-                    <option value="cooking">cooking</option>
-                    <option value="fantasy">fantasy</option>
-                    <option value="history">history</option>
-                    <option value="horror">horror</option>
-                    <option value="it">it</option>
+            <div> 
+                <select onChange={(e) => setSelectedBooksGenre(e.target.value)}>
+                    <option value="none">ALL BOOKS</option>
+                    <option value="Cooking">COOKING</option>
+                    <option value="Fantasy">FANTASY</option>
+                    <option value="History">HISTORY</option>
+                    <option value="Horror">HORROR</option>
+                    <option value="IT">IT</option>
                 </select>
-                <input placeholder="search by title or author"/>
             </div>
+                <input placeholder="search by title or author"/>
+            <div className="visible-movies-container">
+                {
+                    visibleBooks.map( book => (
+                        <div>  
+                            <img 
+                                src={"http://localhost:4000/books_img/" + book.image}
+                                className="movies-movie-icons"
+                                alt="book"                             
+                                />
+                            {book.title}
+                            {book.genre}
+                            {book.author_name}
+                        </div> 
+                                ))
+                            }
+                    </div> 
         </div>
     )
 }
