7fef2deb61bbb8127a9d86c3116982e5fa3cfe97
unknown <acsgyorgy.mail@gmail.com>
2023-03-02 16:59:45 +0100

    bootstrap collapse fixed, working

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -5,7 +5,6 @@ import {ImBooks} from "react-icons/im";
 import { CgShoppingCart } from "react-icons/cg";
 
 
-
 const NavigationBar = () => {
 
     let navigate = useNavigate();
@@ -15,7 +14,7 @@ const NavigationBar = () => {
             <nav class="navbar navbar-expand-lg">
                 <div class="container-fluid">
                     <a class="navbar-brand pt-3 ps-3" href="#">                    
-                        <p className="fs-2 name-of-the-website" onClick={() => {navigate("/")}}>BOOK MANIA</p> 
+                        <p className="fs-1 name-of-the-website" onClick={() => {navigate("/")}}>BOOK MANIA</p> 
                         <ImBooks className="ms-2 fs-1 icon" onClick={() => {navigate("/")}}/>
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
