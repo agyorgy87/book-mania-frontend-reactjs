8db30d957d31112a4c9eb78c1d48a3c990039ba0
unknown <acsgyorgy.mail@gmail.com>
2023-03-02 16:30:36 +0100

    i made some clean code

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -12,34 +12,34 @@ const NavigationBar = () => {
 
     return (  
         <div>
-            <nav class="navbar navbar-expand-lg" style={{ backgroundColor: "#6096B4" }}>
+            <nav class="navbar navbar-expand-lg">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">                    
+                    <a class="navbar-brand pt-3 ps-3" href="#">                    
                         <p className="fs-2 name-of-the-website" onClick={() => {navigate("/")}}>BOOK MANIA</p> 
-                        <ImBooks className="icon" onClick={() => {navigate("/")}}/>
+                        <ImBooks className="ms-2 fs-1 icon" onClick={() => {navigate("/")}}/>
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#" style={{ color: "#EEE9DA"}} onClick={() => {navigate("/")}}>Home</a>
+                        <li class="nav-item navigation-menus">
+                            <a class="nav-link active fs-3" aria-current="page" href="#" style={{ color: "#EEE9DA"}} onClick={() => {navigate("/")}}>Home</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link fs-4" href="#" style={{ color: "#EEE9DA"}} onClick={() => {navigate("/books")}}>Books</a>
+                        <li class="nav-item navigation-menus">
+                            <a class="nav-link fs-3" href="#" style={{ color: "#EEE9DA"}} onClick={() => {navigate("/books")}}>Books</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#" style={{ color: "#EEE9DA"}} onClick={() => {navigate("/contact")}}>Contacts</a>
+                        <li class="nav-item navigation-menus">
+                            <a class="nav-link fs-3" href="#" style={{ color: "#EEE9DA"}} onClick={() => {navigate("/contact")}}>Contacts</a>
                         </li>
                     </ul>
                 </div>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#" style={{ color: "#EEE9DA"}}>Login</a>
+                        <li class="nav-item navigation-menus">
+                            <a class="nav-link fs-3" href="#" style={{ color: "#EEE9DA"}}>Login</a>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item navigation-menus">
                             <a class="nav-link" href="#" style={{ color: "#EEE9DA"}}>< CgShoppingCart className="fs-1"/></a>
                         </li>
                     </ul>
--- src/pages/Books.js
+++ src/pages/Books.js
@@ -6,6 +6,8 @@ const Books = () => {
     const allBooks = useRef([]);
     const [visibleBooks, setVisibleBooks] = useState([]);
     const [selectedBooksGenre, setSelectedBooksGenre] = useState([]);
+    const [filterByPrice, setFilterByPrice] = useState("none");
+    const [filterByReleaseDate, setFilterByReleaseDate] = useState("none");
 
     useEffect(() => {  
 
@@ -16,25 +18,47 @@ const Books = () => {
                 setVisibleBooks(parsedData);
             })
 
-    }, [])
+    }, [visibleBooks])
 
 
     useEffect(() => { 
-
         bookGenreSelection();
-
     },[selectedBooksGenre])
+
+    useEffect(() => { 
+        bookPriceSelection();
+    },[filterByPrice])
       
     
     const bookGenreSelection = () => {
 
         let filterBooksGenre = allBooks.current
-        console.log(filterBooksGenre);
+
         if(selectedBooksGenre !== "none"){   
             filterBooksGenre = filterBooksGenre.filter(book => book.genre === selectedBooksGenre);
             setVisibleBooks(filterBooksGenre); 
         }
         setVisibleBooks(filterBooksGenre);    
+    }
+
+
+    const bookPriceSelection = () => {
+
+        if(filterByPrice === "0-20"){
+            const priceBetweenZeroAndTwenty = allBooks.current.filter(book => book.price >= 0 && book.price <= 20);
+            setVisibleBooks(priceBetweenZeroAndTwenty);
+        }
+        else if(filterByPrice === "21-40"){
+            const priceBetweenTwentyOneAndFourty = allBooks.current.filter(book => book.price >= 21 && book.price <= 40);
+            setVisibleBooks(priceBetweenTwentyOneAndFourty);
+        }
+        else if(filterByPrice === "41-60"){
+            const priceBetweenFourtyOneAndSixty = allBooks.current.filter(book => book.price >= 41 && book.price <= 60);
+            setVisibleBooks(priceBetweenFourtyOneAndSixty);
+        }
+        else if(filterByPrice === "none"){
+            setVisibleBooks(allBooks.current)
+        }
     }
 
     return(
@@ -52,13 +76,11 @@ const Books = () => {
                     <option value="IT">IT</option>
                 </select>
                 <div> 
-                <select>
-                    <option value="none">filter by price</option>
-                    <option value="Cooking"></option>
-                    <option value="Fantasy"></option>
-                    <option value="History"></option>
-                    <option value="Horror"></option>
-                    <option value="IT"></option>
+                <select onChange={(e) => setFilterByPrice(e.target.value)}>
+                    <option value="none">Filter By Price</option>
+                    <option value="0-20">0-20</option>
+                    <option value="21-40">21-40</option>
+                    <option value="41-60">41-60</option>  
                 </select>
             </div>
             <div> 
