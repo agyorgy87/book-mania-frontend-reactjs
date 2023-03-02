5d3246d906ac8af0a2bbcd9bc13c38d92a257ee1
unknown <acsgyorgy.mail@gmail.com>
2023-03-02 15:56:49 +0100

    i recreated navigation bar with bootstrap

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,31 +1,51 @@
 import React from 'react';
 import "../css/NavigationBar.css";
-import {ImBooks} from "react-icons/im";
-import {SlBasket} from "react-icons/sl";
 import { useNavigate } from "react-router-dom";
+import {ImBooks} from "react-icons/im";
+import { CgShoppingCart } from "react-icons/cg";
+
+
 
 const NavigationBar = () => {
 
     let navigate = useNavigate();
 
-    return (
-        <div className="navigation-bar">
-            <div className="logo">
-                <p>BOOK MANIA</p><ImBooks className="icon"/>
-            </div>
-            <div className="navigation-bar-and-basket">
-                <div>
-                    <ul className="navigation-bar-menus">
-                        <li onClick={() => {navigate("/")}}>HOME</li>
-                        <li onClick={() => {navigate("/books")}}>BOOKS</li>
-                        <li onClick={() => {navigate("/contact")}}>CONTACT</li>
+    return (  
+        <div>
+            <nav class="navbar navbar-expand-lg" style={{ backgroundColor: "#6096B4" }}>
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">                    
+                        <p className="fs-2 name-of-the-website" onClick={() => {navigate("/")}}>BOOK MANIA</p> 
+                        <ImBooks className="icon" onClick={() => {navigate("/")}}/>
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#" style={{ color: "#EEE9DA"}} onClick={() => {navigate("/")}}>Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link fs-4" href="#" style={{ color: "#EEE9DA"}} onClick={() => {navigate("/books")}}>Books</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" style={{ color: "#EEE9DA"}} onClick={() => {navigate("/contact")}}>Contacts</a>
+                        </li>
                     </ul>
                 </div>
-                <div>
-                    <p className="sign-up">LOGIN</p>
-                    <SlBasket className="basket-logo"/>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" style={{ color: "#EEE9DA"}}>Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" style={{ color: "#EEE9DA"}}>< CgShoppingCart className="fs-1"/></a>
+                        </li>
+                    </ul>
                 </div>
-            </div>
+                </div>
+            </nav>
         </div>
     )
 }
