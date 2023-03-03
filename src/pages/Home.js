02db4cc0daff56e14f51b10fbf32d85f38ba7d4d
unknown <acsgyorgy.mail@gmail.com>
2023-03-03 13:50:32 +0100

    coupon code div done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -34,6 +34,7 @@ const Home = () => {
             <div>
                 <NavigationBar/>
             </div>
+            {/*
             <div className="main">
                 <div className="coupon-code-container">
                     <div>
@@ -45,6 +46,35 @@ const Home = () => {
                         </div>
                     </div>
                 </div>
+            </div>
+            */}
+            <div className="d-flex justify-content-center">
+                <div className="card mt-5 w-75 square border border-3">
+                    <div className="row g-0" style={{ backgroundColor: "#fffaf0"}}>
+                        <div className="col-md-6 rounded">
+                            <img src={"http://localhost:4000/img/bookread.jpg"} className="img-fluid rounded-start" alt="..." />
+                        </div>
+                        <div className="col-md-6">
+                            <div className="card-body rounded">
+                                <div className="d-flex align-items-center flex-column mb-3">
+                                    <div className="mb-5">
+                                        <p className="card-title fs-1 d-flex justify-content-center discount-font mt-5" style={{ color: "#6096b4"}}>50% COUPON DISCOUNT</p>
+                                        <p className="card-text discount-font">
+                                        When paying for the book, enter your code in the "coupon code" field, so you will receive a 50% discount on your purchased books.
+                                        We wish you a good reading!</p>
+                                        <p className="card-text discount-font">You can find the coupon code in <a href="https://github.com/agyorgy87/book-mania-frontend-reactjs/blob/master/README.md"> my github repository, in CC.md file</a> :)</p>
+                                    </div>
+                                    <div className="d-flex align-items-center flex-column mt-5">
+                                        <p className="card-text discount-font">
+                                        "That's the thing about books. They let you travel without moving your feet."</p>
+                                        <p className="card-text discount-font">- Jhumpa Lahiri</p>   
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
                 <div>  
                     Newness
                     {
@@ -75,7 +105,7 @@ const Home = () => {
                 success books from bestline publishing
                 </div>
                 
-            </div>
+            
         </div>
     );
 }
