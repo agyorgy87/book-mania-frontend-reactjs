f53b8db63db43ea13ed37507bb0b7010b5226b69
unknown <acsgyorgy.mail@gmail.com>
2023-03-06 23:03:23 +0100

    books site sketch done

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,8 +1,10 @@
+import '../css/Books.css';
 import React, { useState, useEffect, useRef } from "react";
 import NavigationBar from '../components/NavigationBar.js';
 
 const Books = () => {
 
+    /*
     const allBooks = useRef([]);
     const [visibleBooks, setVisibleBooks] = useState([]);
     const [selectedBooksGenre, setSelectedBooksGenre] = useState([]);
@@ -60,41 +62,61 @@ const Books = () => {
             setVisibleBooks(allBooks.current)
         }
     }
-
+*/
     return(
         <div>
             <div>
                 <NavigationBar/>
             </div>
-            <div> 
-                <select onChange={(e) => setSelectedBooksGenre(e.target.value)}>
-                    <option value="none">ALL BOOKS</option>
-                    <option value="Cooking">COOKING</option>
-                    <option value="Fantasy">FANTASY</option>
-                    <option value="History">HISTORY</option>
-                    <option value="Horror">HORROR</option>
-                    <option value="IT">IT</option>
-                </select>
-                <div> 
-                <select onChange={(e) => setFilterByPrice(e.target.value)}>
-                    <option value="none">Filter By Price</option>
-                    <option value="0-20">0-20</option>
-                    <option value="21-40">21-40</option>
-                    <option value="41-60">41-60</option>  
-                </select>
-            </div>
-            <div> 
-                <select>
-                    <option value="none">filter by Publication Date</option>
-                    <option value="Cooking"></option>
-                    <option value="Fantasy"></option>
-                    <option value="History"></option>
-                    <option value="Horror"></option>
-                    <option value="IT"></option>
-                </select>
-            </div>
-            </div>
-                <input placeholder="search by title or author"/>
+            <div className="search-site row">
+                <div className="search-lists-container w-25 h-100">
+                    <p>Genres</p>
+                    <ul>
+                    <li>Cooking</li>
+                    <li>Fantasy</li>
+                    <li>History</li>
+                    <li>Horror</li>
+                    <li>IT</li>
+                    </ul>
+                    <p>Price</p>
+                    <ul>
+                    <li>0-20</li>
+                    <li>21-40</li>
+                    <li>41-60</li>
+                    </ul>
+                    <p>Release Date</p>
+                    <ul>
+                    <li>-2000</li>
+                    <li>2001-2010</li>
+                    <li>2011-2020</li>
+                    <li>2020-</li>
+                    </ul>
+                </div>
+                <div className="select-and-results-container w-75 h-100">
+                    <select>{/*onChange={(e) => setSelectedBooksGenre(e.target.value)}*/}
+                        <option value="none">ALL BOOKS</option>
+                        <option value="Cooking">COOKING</option>
+                        <option value="Fantasy">FANTASY</option>
+                        <option value="History">HISTORY</option>
+                        <option value="Horror">HORROR</option>
+                        <option value="IT">IT</option>
+                    </select> 
+                    <select>{/*onChange={(e) => setFilterByPrice(e.target.value)}*/}
+                        <option value="none">Filter By Price</option>
+                        <option value="0-20">0-20</option>
+                        <option value="21-40">21-40</option>
+                        <option value="41-60">41-60</option>  
+                    </select>
+                    <select>
+                        <option value="none">filter by Publication Date</option>
+                        <option value="Cooking"></option>
+                        <option value="Fantasy"></option>
+                        <option value="History"></option>
+                        <option value="Horror"></option>
+                        <option value="IT"></option>
+                    </select>
+                <input placeholder="search by title or author"/>  
+                {/*
             <div className="visible-movies-container">
                 {
                     visibleBooks.map( book => (
@@ -110,8 +132,11 @@ const Books = () => {
                         </div> 
                                 ))
                             }
-                    </div> 
-        </div>
+            </div> 
+                        */}  
+                </div>
+            </div>         
+    </div>   
     )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -62,42 +62,35 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-            {/*
-            <div>  
-                                <img src={"http://localhost:4000/books_img/" + book.image} alt="image"/>
-                                {book.title}
-                                
-                                {book.author_name}
-                            </div>
-            */}
-            <div>
-
-            </div>
-            Newness
-                <div className="row w-100 d-flex justify-content-center">                
-                    {
-                        newness.map((book, index) => (
-                            <div class="card mb-3 border-0" style={{maxWidth: "400px"}}>
-                                <div class="row g-0">
-                                    <div class="col-md-4">
-                                        <img src={"http://localhost:4000/books_img/" + book.image} class="img-fluid rounded-0" alt="..." style={{maxHeight: "100%"}}/>
-                                    </div>
-                                    <div class="col-md-8">
-                                        <div class="card-body pt-0">
-                                            <h5 class="card-text ">{book.title}</h5>
-                                            <p class="card-text">{book.author_name}</p>
-                                            <h4 class="card-text">{book.price} $</h4>
+            <div className="d-flex justify-content-center ms-5">   
+                <div className="row"> 
+                <div> 
+                    <h1 className="">Newness</h1>    
+                    </div> 
+                        {
+                            newness.map((book, index) => (
+                                <div class="card mb-3 me-1 border-0 " style={{maxWidth: "400px"}}>
+                                    <div class="row g-0">
+                                        <div class="col-md-5">
+                                            <img src={"http://localhost:4000/books_img/" + book.image} class="img-fluid rounded-0" alt="..." style={{maxHeight: "100%"}}/>
                                         </div>
-                                        <div className="card-body">
-                                        <p><i class="bi bi-heart me-1"></i>Add To Wishlist</p>
-                                        {/*<i class="bi bi-heart"></i> -> filled heart icon*/}
-                                        <button type="button" className="add-to-cart-buttons">Add To Cart</button>
+                                        <div class="col-md-7">
+                                            <div class="card-body pt-0">
+                                                <h5 class="card-text ">{book.title}</h5>
+                                                <p class="card-text">{book.author_name}</p>
+                                                <h4 class="card-text">{book.price} $</h4>
+                                            </div>
+                                            <div className="card-body">
+                                            <p><i class="bi bi-heart me-1"></i>Add To Wishlist</p>
+                                            {/*<i class="bi bi-heart"></i> -> filled heart icon*/}
+                                            <button type="button" className="add-to-cart-buttons">Add To Cart</button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))
-                    }
+                            ))
+                        }
+                    </div> 
                 </div>
                 <div>
                 stephen king books
