9b5a756cd6de46e3e7dc9ea226c09c5fe1536131
unknown <acsgyorgy.mail@gmail.com>
2023-03-06 23:27:34 +0100

    contact form is almost done

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,8 +7,44 @@ const Contact = () => {
             <div>
                 <NavigationBar/>
             </div>
-            <div>
-                contact
+            <div className="d-flex justify-content-center mt-5">
+                <div>
+                    <div className="d-flex justify-content-center">
+                        <h1>Contact</h1>
+                    </div>
+                    <div className="d-flex justify-content-center">
+                        <div className="me-5">
+                        <form>
+                                <div class="form-group">
+                                    <label for="exampleFormControlInput1">Email address</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleFormControlInput1">Name</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleFormControlTextarea1">Message</label>
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <select class="custom-select" id="inputGroupSelect02">
+                                        <option selected>Choose...</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                    <div class="input-group-append">
+                                        <label class="input-group-text" for="inputGroupSelect02">Question</label>
+                                    </div>
+                                </div>
+                        </form>
+                        </div>
+                        <div>
+                            googlemap
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
