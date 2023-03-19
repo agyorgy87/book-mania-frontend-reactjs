687c95d5b3a6ca25ba0430153c99c356de2ed59f
unknown <acsgyorgy.mail@gmail.com>
2023-03-19 18:40:22 +0100

    book.js page sketch with google map added

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,3 +1,4 @@
+import '../css/Contact.css';
 import React from "react"; 
 import NavigationBar from '../components/NavigationBar.js';
 
@@ -7,45 +8,52 @@ const Contact = () => {
             <div>
                 <NavigationBar/>
             </div>
-            <div className="d-flex justify-content-center mt-5">
-                <div>
-                    <div className="d-flex justify-content-center">
-                        <h1>Contact</h1>
-                    </div>
-                    <div className="d-flex justify-content-center">
-                        <div className="me-5">
-                        <form>
-                                <div class="form-group">
-                                    <label for="exampleFormControlInput1">Email address</label>
-                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleFormControlInput1">Name</label>
-                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleFormControlTextarea1">Message</label>
-                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                                </div>
-                                <div class="input-group mb-3">
-                                    <select class="custom-select" id="inputGroupSelect02">
-                                        <option selected>Choose...</option>
-                                        <option value="1">One</option>
-                                        <option value="2">Two</option>
-                                        <option value="3">Three</option>
-                                    </select>
-                                    <div class="input-group-append">
-                                        <label class="input-group-text" for="inputGroupSelect02">Question</label>
-                                    </div>
-                                </div>
-                        </form>
+            <div className="contact-area">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12 col-sm-12 col-xs-12">
+                            <div className="section-title">
+                                <h3>Contact</h3>
+                            </div>
                         </div>
-                        <div>
-                            googlemap
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6 col-sm-12 col-xs-12">
+                            <div className="contact-form rounded">
+                                <form>
+                                    <div class="form-group mb-4">
+                                        <input type="text" className="form-control" placeholder="Your Name"/>
+                                    </div>
+                                    <div class="form-group mb-4">
+                                        <input type="email" className="form-control" placeholder="Your Email"/>
+                                    </div>
+                                    <div class="form-group mb-4">
+                                        <textarea className="form-control textarea" cols="" rows="" placeholder="Your Message"></textarea>
+                                    </div>
+                                    {/*
+                                    <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+                                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                    </div>
+                                    */}
+                                    <button type="submit" className="btn btn-primary">Submit</button>
+                                    </form>
+                            </div>
+                        </div>
+                        <div className="col-md-6 col-sm-12 col-xs-12">
+                            <div className="map-area">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5391.320466154
+                                502!2d19.050786126615044!3d47.496531975698495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f
+                                13.1!3m3!1m2!1s0x4741dc41b367cca7%3A0x39a14ced90afac34!2sBudapest%2C%20De%C3%A1k%
+                                20Ferenc%20t%C3%A9r%203%2C%201052!5e0!3m2!1sen!2shu!4v1679244151277!5m2!1sen!2shu"
+                                allowfullscreen="" loading="lazy"
+                                ></iframe>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
     )
 }
