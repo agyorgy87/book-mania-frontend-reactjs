7ec6dae72528ca0505989c2bc39babef3cceb3b8
unknown <acsgyorgy.mail@gmail.com>
2023-04-05 21:18:41 +0200

    i created a new coupon

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -37,34 +37,18 @@ const Home = () => {
         <div className="home-page">
             <div>
                 <NavigationBar/>
-            </div>
-            <div className="d-flex justify-content-center">
-                <div className="card mt-5 w-75 square border border-3">
-                    <div className="row g-0" style={{ backgroundColor: "#fffaf0"}}>
-                        <div className="col-md-6 rounded">
-                            <img src={"http://localhost:4000/img/bookread.jpg"} className="img-fluid rounded-start" alt="..." />
+            </div>          
+            <div className="container mt-5">
+                <div className="coupon-card w-75">
+                    <h1>50% COUPON DISCOUNT</h1>
+                        <div>
+                            <p className="card-text discount-font">If you have a coupon code, you can buy books at half price!</p>
+                            <p className="card-text discount-font">You can find the coupon code in <a href="https://github.com/agyorgy87/book-mania-frontend-reactjs/blob/master/README.md"> my github repository, in CC.md file</a> :)</p>
                         </div>
-                        <div className="col-md-6">
-                            <div className="card-body rounded">
-                                <div className="d-flex align-items-center flex-column mb-3">
-                                    <div className="mb-5">
-                                        <p className="card-title fs-1 d-flex justify-content-center discount-font mt-5" style={{ color: "#6096b4"}}>50% COUPON DISCOUNT</p>
-                                        <p className="card-text discount-font">
-                                        When paying for the book, enter your code in the "coupon code" field, so you will receive a 50% discount on your purchased books.
-                                        We wish you a good reading!</p>
-                                        <p className="card-text discount-font">You can find the coupon code in <a href="https://github.com/agyorgy87/book-mania-frontend-reactjs/blob/master/README.md"> my github repository, in CC.md file</a> :)</p>
-                                    </div>
-                                    <div className="d-flex align-items-center flex-column mt-5">
-                                        <p className="card-text discount-font">
-                                        "That's the thing about books. They let you travel without moving your feet."</p>
-                                        <p className="card-text discount-font">- Jhumpa Lahiri</p>   
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <div className="circle1"></div>
+                    <div className="circle2"></div>
                 </div>
-            </div> 
+            </div>
             <div>
                 <div className="d-flex flex-row"> 
                     <h1 className="ps-5">Newness</h1>    
