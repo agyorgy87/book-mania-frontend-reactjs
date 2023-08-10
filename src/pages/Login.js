6710592721b5065796bc83ef789a29b1ddd8442f
unknown <acsgyorgy.mail@gmail.com>
2023-08-10 15:11:31 +0200

    login form design and validate done.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -18,29 +18,54 @@ const Login = () => {
     const [userEmail, setUserEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const [loginMessage, setLoginMessage] = useState("")
-    
-    //const [errorMessage, setErrorMessage] = useState("");
-    //const [success, setSuccess] = useState("");
+    const [loginMessage, setLoginMessage] = useState("");
+
+    const [emailInputError, setEmailInputError] = useState(false);
+    const [emailInputErrorMessage, setEmailInputErrorMessage] = useState(false);
+
+    const [passwordInputError, setPasswordInputError] = useState(false);
+    const [passwordInputErrorMessage, setPasswordInputErrorMessage] = useState(false);
 
     const [touched, setTouched] = useState(false);
-
+    
     const [passwordShow, setPasswordShow] = useState(false);
 
     const [passwordInfo, setPasswordInfo] = useState(false);
 
+
+    useEffect(() => {
+        if(userEmail === "" && password !== "") {
+            setEmailInputErrorMessage(true);
+        }
+    },[userEmail,password])
+
+
+    useEffect(() => {
+        if(userEmail.length > 0) {
+            setEmailInputErrorMessage(false);
+            setEmailInputError(false);
+        }
+    }, [userEmail])
+    
+
     useEffect(() => {
         if(password === "") {
-            setPasswordInfo(false);
+            setPasswordInfo(false);  
+            setPasswordInputErrorMessage(false);
         }
+        if(password.length > 0){
+            setPasswordInfo(true);
+            setPasswordInputError(false);
+            setPasswordInputErrorMessage(false); 
+        }   
     },[password])
+    
 
     const togglePassword = () => {
         setPasswordShow(!passwordShow); 
     }
 
     const login = (e) => {
-
     if(userEmail === "" && password === ""){
         setLoginMessage("")
     }
@@ -55,12 +80,28 @@ const Login = () => {
         .then(response => {
             if(response.data.error) {
                 if(userEmail === "" && password === ""){
-                    setLoginMessage("Fill in the fields to login.");
-                }else{
-                    setLoginMessage("Invalid Email address or Password. Please try again."); 
+                    setPasswordInfo(false);
+                    setEmailInputError(true);
+                    setEmailInputErrorMessage(true);
+                    setPasswordInputError(true);
+                    setPasswordInputErrorMessage(true);
+                }else if(userEmail !== "" && password === ""){
+                    setPasswordInputError(true);
+                    setPasswordInputErrorMessage(true);
+                }else if(userEmail === "" && password !== ""){
+                    setEmailInputError(true);
+                    setEmailInputErrorMessage(true);
+                }else{               
+                    setLoginMessage("Invalid Email address or Password. Please try again.");
+                    setEmailInputError(true);
+                    setEmailInputErrorMessage(false);
+                    setPasswordInfo(false);
+                    setPasswordInputError(true);
                 }   
             }
             else{
+                setEmailInputError(false);
+                setPasswordInputError(false);
                 setLoginMessage("Successful Login.");
                 let stringifiedToken = JSON.stringify(response.data);
                 localStorage.setItem("token", stringifiedToken);
@@ -69,19 +110,6 @@ const Login = () => {
             }
         })      
     }
-
-    let emptyEmailInputErrorMessage;
-
-    const emptyInputError = () => {
-        if(userEmail === "" && password === ""){
-            emptyEmailInputErrorMessage = false;
-        }
-        else if(userEmail === "" && password !== ""){
-            emptyEmailInputErrorMessage = true;
-        }
-    }
-
-    emptyInputError(); 
 
     return (
         <div className="login-page">
@@ -104,57 +132,63 @@ const Login = () => {
                                 type="email" 
                                 aria-describedby="emailHelp" 
                                 id="InputEmail" 
-                                className={`form-control email-password-input ${emptyEmailInputErrorMessage ? 'email-input-alert' : ''}`}
+                                className={`form-control email-password-input 
+                                ${emailInputError ? 'input-alert' : '' } 
+                                ${emailInputErrorMessage ? 'input-alert' : ''}`}
                                 value={userEmail} 
                                 onBlur={() => setTouched(true)} 
                                 onChange={(e) => setUserEmail(e.target.value)}
                             />
                         </div>                                           
-                        {
-                        touched ? 
-                            (emptyEmailInputErrorMessage ? 
+                        {   touched ? 
+                                (emailInputErrorMessage ? 
+                                    <div className="mt-1 ms-5 me-5 d-flex">
+                                        <AiOutlineExclamationCircle className="alert-mark fs-5 me-1"/>
+                                            <p className="alert-message">Please enter an email.</p>
+                                    </div>  
+                                :
+                                null)
+                            :
+                            null
+                        }    
+                        {   loginMessage ? 
                                 <div className="mt-1 ms-5 me-5 d-flex">
-                                    <AiOutlineExclamationCircle className="alert-mark fs-5 me-1"/><p className="email-not-filled-text">The email address is not filled.</p>
-                                </div>  
+                                    <AiOutlineExclamationCircle className="login-alert-mark fs-3 me-1"/>
+                                    <p className="alert-message">{loginMessage}</p>
+                                </div>
                                 :
                                 null
-                                )
-                        :
-                        null
-                        }                       
+                        }                  
                         <div className="ms-5 me-5 mt-4 email-password-container">
                                 <label htmlFor="InputPassword" className="form-label email-password-label">Password</label>
                             <div className="password-container">                               
                                 <input 
                                     type={passwordShow ? "text" : "password"} 
-                                    className="form-control email-password-input" 
+                                    className={`form-control email-password-input ${passwordInputError ? 'input-alert' : ''}`}
                                     id="InputPassword" 
                                     onChange={(e) => {setPassword(e.target.value); setPasswordInfo(true)} }
                                 />                   
-                                    { 
-                                    passwordShow ?
-                                    <i className="eye-icons bi bi-eye fs-4" onClick={togglePassword}></i>                                      
-                                    :
-                                    <i className="eye-icons bi bi-eye-slash fs-4" onClick={togglePassword}></i>
+                                    {   passwordShow ?
+                                            <i className="eye-icons bi bi-eye fs-4" onClick={togglePassword}></i>                                      
+                                            :
+                                            <i className="eye-icons bi bi-eye-slash fs-4" onClick={togglePassword}></i>
                                     }
-                                    { passwordInfo ?
-                                        <p className="password-help">Your password must be least 8 characters and minimum 1 number.</p>
-                                    :
-                                        null
+                                    {   passwordInfo ?
+                                            <p className="password-help">Your password must be least 8 characters and minimum 1 number.</p>
+                                            :
+                                            null
+                                    } 
+                                    {   passwordInputErrorMessage ?
+                                            <div className="d-flex mt-1">
+                                                <AiOutlineExclamationCircle className="alert-mark fs-5 me-1"/>
+                                                <p className="alert-message">Please enter a password.</p>
+                                            </div>  
+                                            :
+                                            null
                                     }                                               
                             </div>
-                        </div>
-                        <div className="ms-5 me-5 mt-2">
-                            {
-                                loginMessage ? 
-                                    <div className="">
-                                        {loginMessage}
-                                    </div>
-                            :
-                                null
-                            }
-                        </div>                
-                        <div className="login-button-container mt-5 ms-5 me-5 mb-4">
+                        </div>             
+                        <div className={`login-button-container mt-5 ms-5 me-5 mb-4`}>
                             <button type="submit" className="btn w-100 fs-5 login-button">Sign in</button>
                         </div>
                         <div className="d-flex justify-content-center mb-4">
@@ -162,7 +196,8 @@ const Login = () => {
                         </div>
                         <hr className="mt-2 mb-3"/>
                         <div className="d-flex justify-content-center">
-                            <p className="mt-3 me-2 fs-5 fw-lighter ask-account">Have an account?</p> <Link className="sign-up-link mt-3 fs-5 fw-bold" to={"/createaccount"}>Sign up</Link>
+                            <p className="mt-3 me-2 fs-5 fw-lighter ask-account">Have an account?</p> 
+                            <Link className="sign-up-link mt-3 fs-5 fw-bold" to={"/createaccount"}>Sign up</Link>
                         </div>
                     </form>
                 </div>       
