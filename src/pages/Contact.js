6eba79a1fb2e72f6f059dd47f903f26e31a71933
unknown <acsgyorgy.mail@gmail.com>
2023-08-18 09:29:25 +0200

    alert-mark added to ExistingEmailAddress.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -114,10 +114,7 @@ const Contact = () => {
                         </div>
                         <div className="col-md-6 col-sm-12 col-xs-12">
                             <div className="map-area">
-                                <iframe title="address-point" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5391.320466154
-                                502!2d19.050786126615044!3d47.496531975698495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f
-                                13.1!3m3!1m2!1s0x4741dc41b367cca7%3A0x39a14ced90afac34!2sBudapest%2C%20De%C3%A1k%
-                                20Ferenc%20t%C3%A9r%203%2C%201052!5e0!3m2!1sen!2shu!4v1679244151277!5m2!1sen!2shu"
+                                <iframe title="address-point" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2694.8402062041205!2d19.07995727675107!3d47.51250309447013!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741dc7ed6d66397%3A0xc13fd2d58ab64c29!2sBudapest%2C%20V%C3%A1rosliget%2C%201146!5e0!3m2!1sen!2shu!4v1692280477141!5m2!1sen!2shu"
                                 allowFullScreen="" loading="lazy"
                                 />
                             </div>
--- src/popup/ExistingEmailAddress.js
+++ src/popup/ExistingEmailAddress.js
@@ -1,13 +1,17 @@
 import "../css/ExistingEmailAddress.css";
 import React from "react";
-
-
+import { GoAlert } from "react-icons/go";
 
 const ExistingEmailAddress = (props) => { 
 
     return (
         <div className="overlay">
             <form className="form existing-email-container d-flex flex-column shadow-lg p-3 bg-body-tertiary rounded">
+                <div className="text-center"> 
+                    <div>
+                        <GoAlert className="alert-icon fs-1"/>
+                    </div>
+                </div>
                 <div className="d-flex justify-content-center mb-1">
                     <h4 className="alert-modal-text text-center pt-2">Already registered with this email address.</h4>
                 </div>
